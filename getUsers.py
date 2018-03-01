8ea7272cd7507d8c1d21ed497b59b79f44d3d1f7
Ian Bloom <ian.bloom@gmail.com>
2018-03-01 15:48:33 -0800

    Add files via upload
    getUsers.py loops through all .xml files in directory and imports them as datasources into LogicMonitor

--- getUsers.py
+++ getUsers.py
@@ -6,6 +6,7 @@ import hashlib
 import base64
 import time
 import hmac
+import os
 
 #Account Info
 
@@ -16,17 +17,29 @@ AccessId ='dSpe6j9eTQXs3Iph7jCU'
 AccessKey ='dcm!p2d2w79V=5f}+[354xL=g{k442Y6h5qV}C_6'
 Company = 'ianbloom'
 
-#File Information
-fileName = '/Users/ianbloom/Documents/massImport/Noisiest_DataSources.xml'
-#fileName = 'Noisiest_DataSources.xml'
-file = open(fileName,'r')
-xml = file.read()
+#Initialize fileArray variable to hold filepath for all dataSources
+currentWorkingDirectory = os.getcwd()
+fileArray = []
+hmacArray = []
 
-#Request Info
-httpVerb ='POST'
-resourcePath = '/setting/datasources/importxml'
-queryParams =''
-data = '''------WebKitFormBoundary12345BUlKhOAAA1X
+#Iterate through files in CWD, if file ends in .xml then assume it is a datasource and add it to fileArray
+for filename in os.listdir(currentWorkingDirectory):
+	if filename.endswith(".xml"):
+		fileArray.append(os.path.join(currentWorkingDirectory,filename))
+	else:
+		continue
+
+for i in range(len(fileArray)):
+	#File Information
+	fileName = fileArray[i]
+	file = open(fileName,'r')
+	xml = file.read()
+
+	#Request Info
+	httpVerb ='POST'
+	resourcePath = '/setting/datasources/importxml'
+	queryParams =''
+	data = '''------WebKitFormBoundary12345BUlKhOAAA1X
 Content-Disposition: form-data; name="file"; filename="''' + fileName + '''"
 Content-Type: text/xml
 
@@ -34,25 +47,29 @@ Content-Type: text/xml
 
 ------WebKitFormBoundary12345BUlKhOAAA1X--'''
 
-#Construct URL 
-url = 'https://' + Company + '.logicmonitor.com/santaba/rest' + resourcePath + queryParams
+	#Construct URL 
+	url = 'https://' + Company + '.logicmonitor.com/santaba/rest' + resourcePath + queryParams
 
-#Get current time in milliseconds
-epoch = str(int(time.time() * 1000))
+	#Get current time in milliseconds
+	epoch = str(int(time.time() * 1000))
 
-#Concatenate Request details
-requestVars = httpVerb + epoch + data + resourcePath
+	#Concatenate Request details
+	requestVars = httpVerb + epoch + data + resourcePath
 
-hmac = hmac.new(AccessKey.encode(),msg=requestVars.encode(),digestmod=hashlib.sha256).hexdigest()
-signature = base64.b64encode(hmac.encode())
+	hmacArray.append(hmac.new(AccessKey.encode(),msg=requestVars.encode(),digestmod=hashlib.sha256).hexdigest())
+	signature = base64.b64encode(hmacArray[i].encode())
 
-#Construct headers
-auth = 'LMv1 ' + AccessId + ':' + signature.decode() + ':' + epoch
-headers = {'Content-Type':'multipart/form-data; boundary=----WebKitFormBoundary12345BUlKhOAAA1X','Authorization':auth}
+	#Construct headers
+	auth = 'LMv1 ' + AccessId + ':' + signature.decode() + ':' + epoch
+	headers = {'Content-Type':'multipart/form-data; boundary=----WebKitFormBoundary12345BUlKhOAAA1X','Authorization':auth}
 
-#Make request
-response = requests.post(url, data=data, headers=headers)
+	#Make request
+	#print('url= ', url)
+	#print('data=', data)
+	#print('headers= ', headers)
 
-#Print status and body of response
-print('Response Status:',response.status_code)
-print('Response Body:',response.content)
+	response = requests.post(url, data=data, headers=headers)
+
+	#Print status and body of response
+	print('Response Status:',response.status_code)
+	print('Response Body:',response.content)
